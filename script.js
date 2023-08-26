11c0534bd93ff582b905b47cc800f16c318e9b4e
shuklaadity <aditya02shukla1@gmail.com>
2023-08-26 09:01:53 +0530

    netflix clone

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var btn= document.querySelectorAll(".ans");
+var ans1=document.querySelectorAll(".question");
+
+
+
+function show(){
+   let ans = document.getElementById("hi");
+   console.log('ello')
+   ans.classList.toggle("big");
+
+}
