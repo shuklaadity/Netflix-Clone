2fed0b40913e1e032e271dd05d10e48be67cce4e
shuklaadity <aditya02shukla1@gmail.com>
2023-09-03 15:00:03 +0530

    add new features

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ var ans1=document.querySelectorAll(".question");
 
 
 
-function show(){
-   let ans = document.getElementById("hi");
-   console.log('ello')
-   ans.classList.toggle("big");
+function show1(){
+   let ans = document.getElementById("hi1");
+    ans.classList.toggle("big");
+
+}
+function show2(){
+   let ans = document.getElementById("hi2");
+    ans.classList.toggle("big");
+
+}
+function show3(){
+   let ans = document.getElementById("hi3");
+    ans.classList.toggle("big");
+
+}
+function show4(){
+   let ans = document.getElementById("hi4");
+    ans.classList.toggle("big");
+
+}
+function show5(){
+   let ans = document.getElementById("hi5");
+    ans.classList.toggle("big");
 
 }
